27347562dc38f866c23a1f4abf4548a88404db39
lucea97217 <lucea.lenny@hotmail.com>
2022-04-14 12:32:47 +0200

    .

--- CExcreationDATA.R
+++ CExcreationDATA.R
@@ -1,6 +1,7 @@
 library(data.table)
 library(dplyr)
 library(tidyr)
+library(BenfordTests)
 df <-read.csv("covidALL.csv", sep= ";", header= T)
 data.table(df)
 distinct(df,df$nom,.keep_all = T)
@@ -11,3 +12,14 @@ dfrance <-df[df$nom=="france",]
 
 
 write.csv(dfrance %>% drop_na(),"datafrance.csv")
+
+# exemple sélection taîwan
+df <-read.csv("corona_comp.csv", sep= ";", header= T)
+data.table(df)
+distinct(df,df$Pays,.keep_all = T)
+dchine <- df[df$Pays=="Taïwan",]
+x <- c(dchine$Infections,dchine$deces,dchine$Guerisons)
+
+# affichage des deux premiers chiffres significatifs
+signifd(x , digits=2)
+# on a plus qu'a virer si on veut les valeurs en 0 qui ne sont pas prises en compte.
